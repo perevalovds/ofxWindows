2c44aadd2a1bb60b437c03deec9b004613877dec
perevalovds <perevalovds@gmail.com>
2019-10-13 15:08:06 +0500

    example of sending keypress to Notepad

---
+++ example_keysend/src/ofApp.cpp
@@ -0,0 +1,88 @@
+#include "ofApp.h"
+#include "ofxWindows.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofSetWindowShape(200, 100);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	float time = ofGetElapsedTimef();
+	if (time - time_ >= 1) {
+		time_ = time;
+		string window_class = "Notepad";
+		char c = k + '0';
+		bool result = ofxWindows::SendKeyboardByWindowClass("Notepad", c);
+		if (result) {
+			cout << "Sent " << c << " to " << window_class << endl;
+		}
+		else {
+			cout << "Can't sent symbol, may be window with class " << window_class << " isn't exists"<< endl;
+			cout << "To check this, run unitilies/winlister" << endl;
+		}
+		k = (k+1) % 10;
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ example_keysend/src/ofApp.h
@@ -0,0 +1,29 @@
+#pragma once
+
+//ofxWindows, example of sending keypresses to Notepad.
+//To check it, run Notepad.
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp {
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	float time_ = 0;
+	int k = 0;
+};
